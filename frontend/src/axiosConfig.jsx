20afa4e1d5ff895b5a00b89fef0610e076edb00c
Phuc2405 <pnthienphuc2405@gmail.com>
2026-04-03 07:16:36 +0000

    Revise MONGO_URI and change baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.239.227.205:5001', // live
+  baseURL: 'http://3.106.252.69:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
